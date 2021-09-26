cc104542cb48859eff40b4306b4a46f1369943cb
CaptSoulBeard <Salim.Al.Ateeqi1@gmail.com>
2021-09-26 20:32:40 +0300

    Calculator does not print results

--- index.js
+++ index.js
@@ -7,19 +7,37 @@
  * If you click on button +, the text will be "+"
  * ,... and so on
  */
+
+let array = [];
+let operator;
+
 function buttonClick(text) {
-  if (
-    text !== 'AC' &&
-    text !== '=' &&
-    text !== '-' &&
-    text !== 'x' &&
-    text !== '+' &&
-    text !== '.' &&
-    text !== '±' &&
-    text !== '÷' &&
-    text !== '%'
-  ) {
+  if (!isNaN(text)) {
     printOnConsole(text);
+    if (array.length < 2) {
+      array.push(text);
+      console.log(array);
+      console.log(array.length);
+    }
+  } else if (text === "AC") {
+    printOnConsole(0);
+  } else if (text !== "AC" && isNaN(text)) {
+    operator = text;
+    console.log("Step 1");
+  }
+
+  let result;
+  console.log("Length of array", array.length);
+  if (array.length > 1) {
+    if (operator === "+") {
+      console.log(array);
+      result = array[0] + array[1];
+
+      console.log("Step 3");
+    }
+  }
+  if (text === "=") {
+    printOnConsole(result);
   }
 }
 
@@ -29,5 +47,5 @@ function buttonClick(text) {
  */
 
 // Remove Me after testing
-printOnConsole('123');
-updateHistory(['This is a sample historry', '1 + 5 = 6', '5 x 10 = 50']);
+printOnConsole("123");
+updateHistory(["This is a sample history", "1 + 5 = 6", "5 x 10 = 50"]);
