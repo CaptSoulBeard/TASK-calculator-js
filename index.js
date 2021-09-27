ccfe13dc85a79a1fdb28126e20390452daf2cec6
CaptSoulBeard <76257198+CaptSoulBeard@users.noreply.github.com>
2021-09-27 10:40:04 +0300

    Working Calculator that needs a lot of polishing

--- index.js
+++ index.js
@@ -10,36 +10,82 @@
 
 let array = [];
 let operator;
+let firstNumber;
+let secondNumber;
 
 function buttonClick(text) {
-  if (!isNaN(text)) {
+  // if (!isNaN(text)) {
+  //   printOnConsole(text);
+  //   if (array.length < 2) {
+  //     array.push(text);
+  //     console.log(array);
+  //     console.log(array.length);
+  //   }
+  // } else if (text === "AC") {
+  //   printOnConsole(0);
+  // } else if (text !== "AC" && isNaN(text)) {
+  //   operator = text;
+  //   console.log("Step 1");
+
+  // printOnConsole(typeof text);
+
+  if (Number(text)) {
     printOnConsole(text);
-    if (array.length < 2) {
-      array.push(text);
-      console.log(array);
-      console.log(array.length);
+    array.push(text);
+    firstNumber = array[0];
+    secondNumber = array[1];
+
+    console.log(array);
+    console.log(`Your first number is: ${firstNumber}`);
+    console.log(`Your second number is: ${secondNumber}`);
+  } else if (
+    text === "±" ||
+    text === "%" ||
+    text === "÷" ||
+    text === "x" ||
+    text === "-" ||
+    text === "+"
+  ) {
+    operator = text;
+    console.log(operator);
+  } else if (text === "=") {
+    if (operator === "±") {
+      printOnConsole(``);
+    } else if (operator === "%") {
+      printOnConsole(``);
+    } else if (operator === "÷") {
+      result = firstNumber / secondNumber;
+      printOnConsole(result);
+    } else if (operator === "x") {
+      printOnConsole(firstNumber * secondNumber);
+    } else if (operator === "-") {
+      printOnConsole(firstNumber - secondNumber);
+    } else if (operator === "+") {
+      printOnConsole(firstNumber + secondNumber);
     }
   } else if (text === "AC") {
     printOnConsole(0);
-  } else if (text !== "AC" && isNaN(text)) {
-    operator = text;
-    console.log("Step 1");
-  }
-
-  let result;
-  console.log("Length of array", array.length);
-  if (array.length > 1) {
-    if (operator === "+") {
-      console.log(array);
-      result = array[0] + array[1];
-
-      console.log("Step 3");
-    }
-  }
-  if (text === "=") {
-    printOnConsole(result);
+    array.splice(0, array.length);
+    operator = undefined;
+    console.log(array);
+    console.log(operator);
   }
 }
+
+//   let result;
+//   console.log("Length of array", array.length);
+//   if (array.length > 1) {
+//     if (operator === "+") {
+//       console.log(array);
+//       result = array[0] + array[1];
+
+//       console.log("Step 3");
+//     }
+//   }
+//   if (text === "=") {
+//     printOnConsole(result);
+//   }
+// }
 
 /** Supporting functions
  * 1. `printOnConsole(text)`, takes any text, and renders the console part of the web page
